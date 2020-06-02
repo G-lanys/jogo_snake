2a88fc869cff03bdee2ac59bd28195bd37fa383a
G-lanys <goesalane@gmail.com>
2020-06-02 11:33:15 -0300

    Definindo o movimento que a cobrinha irá fazer

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
 
 function criarBG(){
     context.fillStyle = "lightblack";
@@ -19,6 +21,33 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){    
+    criarBG();
+    criarCobrinha();
+
+    /* setar ponto de partida da cobrinha */
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    /*cooerdenadas ca cobrinha*/
+    if(direction =="right") snakeX+= box;
+    if(direction =="left") snakeX -= box;
+    if(direction =="up") snakeY -= box;
+    if(direction =="down") snakeY += box;
+
+    /* add função pop para retiar i ultimo elemento do arry*/
+    snake.pop();
+
+    /*inserindo a nova cabeça da cobrinha com o unshifit*/
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
 
